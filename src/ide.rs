c0d477e3909aa6df923debca6f9a41eec32b52f4
Niko Matsakis <niko@alum.mit.edu>
2021-12-12 08:34:19 -0500

    add command line options

--- src/ide.rs
+++ src/ide.rs
@@ -1,4 +1,8 @@
-pub fn main() -> eyre::Result<()> {
+#[derive(structopt::StructOpt)]
+pub struct Options {}
+
+pub fn main(crate_options: &crate::Options, options: &Options) -> eyre::Result<()> {
     let mut server = dada_lsp::LspServer::new()?;
-    server.main_loop()
+    server.main_loop()?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+use structopt::StructOpt;
+
 mod ide;
 
+#[derive(StructOpt)]
+pub struct Options {
+    #[structopt(subcommand)] // Note that we mark a field as a subcommand
+    cmd: Command,
+}
+
+#[derive(StructOpt)]
+pub enum Command {
+    /// Pound acorns into flour for cookie dough.
+    Ide(ide::Options),
+}
+
 fn main() -> eyre::Result<()> {
-    ide::main()
+    let options = Options::from_args();
+    match &options.cmd {
+        Command::Ide(command_options) => {
+            ide::main(&options, command_options)?;
+        }
+    }
+    Ok(())
 }
