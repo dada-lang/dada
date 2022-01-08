214159e1732bdec70302974f851a9e5fae8b129e
Remy Rakic <remy.rakic+github@gmail.com>
2022-01-08 01:06:29 +0100

    Add `DebugWithDb` impls for some of the syntax nodes

--- components/dada-ir/src/format_string.rs
+++ components/dada-ir/src/format_string.rs
@@ -19,6 +19,18 @@ impl FormatString {
     }
 }
 
+impl<Db: ?Sized + crate::Db> salsa::DebugWithDb<Db> for FormatString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>, db: &Db) -> std::fmt::Result {
+        salsa::DebugWithDb::fmt(self.data(db), f, db)
+    }
+}
+
+impl<Db: ?Sized + crate::Db> salsa::DebugWithDb<Db> for FormatStringData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>, db: &Db) -> std::fmt::Result {
+        salsa::DebugWithDb::fmt(&self.sections, f, db)
+    }
+}
+
 #[salsa::interned(FormatStringSection in crate::Jar)]
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum FormatStringSectionData {
@@ -34,6 +46,25 @@ impl FormatStringSection {
         match self.data(db) {
             FormatStringSectionData::Text(w) => w.len(db),
             FormatStringSectionData::TokenTree(tree) => tree.len(db),
+        }
+    }
+}
+
+impl<Db: ?Sized + crate::Db> salsa::DebugWithDb<Db> for FormatStringSection {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>, db: &Db) -> std::fmt::Result {
+        salsa::DebugWithDb::fmt(self.data(db), f, db)
+    }
+}
+
+impl<Db: ?Sized + crate::Db> salsa::DebugWithDb<Db> for FormatStringSectionData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>, db: &Db) -> std::fmt::Result {
+        match self {
+            FormatStringSectionData::Text(word) => {
+                f.debug_tuple("Text").field(&word.debug(db)).finish()
+            }
+            FormatStringSectionData::TokenTree(tree) => {
+                f.debug_tuple("TokenTree").field(&tree.debug(db)).finish()
+            }
         }
     }
 }
