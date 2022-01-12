c88491a28fcb68141a593210ffefdf956a2e625e
Brian Anderson <andersrb@gmail.com>
2022-01-12 15:48:26 -0800

    dada-web: Display output on load

--- components/dada-web/index.js
+++ components/dada-web/index.js
@@ -8,13 +8,19 @@ init()
         });
         editor.session.on('change', async function (delta) {
             // delta.start, delta.end, delta.lines, delta.action
-            try {
-                let text = await execute(editor.getValue());
-                var ansi_up = new AnsiUp;
-                var html = ansi_up.ansi_to_html(text);
-                var cdiv = document.getElementById("output");
-                cdiv.innerHTML = html;
-            } catch (e) { }
+            await updateOutput(editor);
         });
         // editor.session.setMode("ace/mode/javascript");
+
+        await updateOutput(editor);
     });
+
+async function updateOutput(editor) {
+    try {
+        let text = await execute(editor.getValue());
+        var ansi_up = new AnsiUp;
+        var html = ansi_up.ansi_to_html(text);
+        var cdiv = document.getElementById("output");
+        cdiv.innerHTML = html;
+    } catch (e) { }
+}
