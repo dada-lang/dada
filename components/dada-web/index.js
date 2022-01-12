6b4dba4fe079fb0806fbdb3d9dffcebcae73e92c
Niko Matsakis <niko@alum.mit.edu>
2022-01-12 04:17:09 -0500

    get playground working

--- components/dada-web/index.js
+++ components/dada-web/index.js
@@ -5,8 +5,13 @@ init()
         editor.setTheme("ace/theme/twilight");
         editor.session.on('change', async function (delta) {
             // delta.start, delta.end, delta.lines, delta.action
-            console.log(`text is ${editor.getValue()}`);
-            console.log(await execute(editor.getValue()));
+            try {
+                let text = await execute(editor.getValue());
+                var ansi_up = new AnsiUp;
+                var html = ansi_up.ansi_to_html(text);
+                var cdiv = document.getElementById("output");
+                cdiv.innerHTML = html;
+            } catch (e) { }
         });
         // editor.session.setMode("ace/mode/javascript");
     });
