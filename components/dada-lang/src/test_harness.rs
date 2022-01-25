9f9b0c83d56bb69cf9b2adc1a1bbda01cd89affe
zhoufan <1247714429@qq.com>
2022-01-25 17:46:36 +0800

    make fmt happy

--- components/dada-lang/src/test_harness.rs
+++ components/dada-lang/src/test_harness.rs
@@ -516,8 +516,10 @@ enum QueryKind {
 fn expected_diagnostics(path: &Path) -> eyre::Result<Vec<ExpectedDiagnostic>> {
     let file_contents = std::fs::read_to_string(path)?;
 
-    let re =
-        regex::Regex::new(r"^(?P<prefix>[^#]*)#!\s*(?P<highlight>\^+)?\s*(?P<severity>[^\s*]+)\s*(?P<msg>.*)").unwrap();
+    let re = regex::Regex::new(
+        r"^(?P<prefix>[^#]*)#!\s*(?P<highlight>\^+)?\s*(?P<severity>[^\s*]+)\s*(?P<msg>.*)",
+    )
+    .unwrap();
 
     let mut last_code_line = 1;
     let mut result = vec![];
