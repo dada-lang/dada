1915048bb05ec349d5a99939ae537b4c7db76ac2
Rémy Rakic <remy.rakic+github@gmail.com>
2022-02-27 21:45:13 +0100

    introduce error formatting options

--- components/dada-error-format/src/format.rs
+++ components/dada-error-format/src/format.rs
@@ -1,24 +1,48 @@
 use std::io::Cursor;
 
-use ariadne::{Label, Report, ReportKind, Source};
+use ariadne::{Config, Label, Report, ReportKind, Source};
 use dada_ir::filename::Filename;
+
+/// Options for controlling error formatting when they are printed.
+#[derive(Clone, Copy)]
+pub struct FormatOptions {
+    /// Whether or not errors should use rich formatting with colors. This is generally turned on,
+    /// except in tests, where the escape codes obscure the error messages.
+    with_color: bool,
+}
+
+impl FormatOptions {
+    pub fn no_color() -> Self {
+        Self { with_color: false }
+    }
+}
+
+const DEFAULT_FORMATTING: FormatOptions = FormatOptions { with_color: true };
 
 pub fn print_diagnostic(
     db: &dyn crate::Db,
     diagnostic: &dada_ir::diagnostic::Diagnostic,
 ) -> eyre::Result<()> {
-    Ok(ariadne_diagnostic(db, diagnostic)?.print(SourceCache::new(db))?)
+    Ok(ariadne_diagnostic(db, diagnostic, DEFAULT_FORMATTING)?.print(SourceCache::new(db))?)
 }
 
 pub fn format_diagnostics(
     db: &dyn crate::Db,
     diagnostics: &[dada_ir::diagnostic::Diagnostic],
 ) -> eyre::Result<String> {
+    format_diagnostics_with_options(db, diagnostics, DEFAULT_FORMATTING)
+}
+
+pub fn format_diagnostics_with_options(
+    db: &dyn crate::Db,
+    diagnostics: &[dada_ir::diagnostic::Diagnostic],
+    options: FormatOptions,
+) -> eyre::Result<String> {
     let mut output = Vec::new();
     let mut cursor = Cursor::new(&mut output);
     let mut cache = SourceCache::new(db);
     for diagnostic in diagnostics {
-        let ariadne = ariadne_diagnostic(db, diagnostic)?;
+        let ariadne = ariadne_diagnostic(db, diagnostic, options)?;
         ariadne.write(&mut cache, &mut cursor)?;
     }
     Ok(String::from_utf8(output)?)
@@ -27,13 +51,15 @@ pub fn format_diagnostics(
 fn ariadne_diagnostic(
     _db: &dyn crate::Db,
     diagnostic: &dada_ir::diagnostic::Diagnostic,
+    options: FormatOptions,
 ) -> eyre::Result<ariadne::Report<ASpan>> {
     let mut builder = Report::<ASpan>::build(
         ReportKind::Error,
         diagnostic.span.filename,
         diagnostic.span.start.into(),
     )
-    .with_message(&diagnostic.message);
+    .with_message(&diagnostic.message)
+    .with_config(Config::default().with_color(options.with_color));
 
     for label in &diagnostic.labels {
         builder = builder.with_label(Label::new(ASpan(label.span())).with_message(&label.message));
