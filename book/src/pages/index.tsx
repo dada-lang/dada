da5fb58c22d97960f9e70be33614eb68fd3859df
XFFXFF <1247714429@qq.com>
2022-05-04 16:23:37 +0800

    playground: class -> className in react

--- book/src/pages/index.tsx
+++ book/src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 // import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Ide from '@site/src/components/Ide';
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
@@ -19,12 +17,12 @@ function HomepageHeader() {
             <img src="https://raw.githubusercontent.com/dada-lang/dada-artwork/main/dada.svg" width="600"></img>
           </Col>
           <Col>
-            <blockquote class="rectangle-speech-border">
-              <h1 class="dada-left-justify">Welcome to <b>Dada</b>, an experimental new programming language!</h1>
+            <blockquote className="rectangle-speech-border">
+              <h1 className="dada-left-justify">Welcome to <b>Dada</b>, an experimental new programming language!</h1>
 
-              <h3 class="dada-left-justify"><a href="/docs/dyn_tutorial">Care to try our live tutorial?</a></h3>
-              <h3 class="dada-left-justify"><a href="/docs/about">Wondering what the heck Dada is all about?</a></h3>
-              <h3 class="dada-left-justify"><a href="/playground">Jump to the web playground?</a></h3>
+              <h3 className="dada-left-justify"><a href="/docs/dyn_tutorial">Care to try our live tutorial?</a></h3>
+              <h3 className="dada-left-justify"><a href="/docs/about">Wondering what the heck Dada is all about?</a></h3>
+              <h3 className="dada-left-justify"><a href="/playground">Jump to the web playground?</a></h3>
             </blockquote>
           </Col>
         </Row>
