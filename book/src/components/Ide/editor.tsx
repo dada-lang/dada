91c52d7022633c560911160456b3610fb41dd6a8
Bernardo <berublan@gmail.com>
2022-05-16 16:00:50 +0200

    fix ace theme loading and use rust mode

--- book/src/components/Ide/editor.tsx
+++ book/src/components/Ide/editor.tsx
@@ -1,32 +1,40 @@
 import React, { PropsWithChildren } from "react";
 import AceEditor from "react-ace";
+import { useColorMode } from "@docusaurus/theme-common";
 
-import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-rust";
 import "ace-builds/src-noconflict/theme-github";
-// import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/theme-twilight";
 
 import type { Cursor } from ".";
 
 type OutputProps = {
-  source: string;
-  onCursorChange: (c: Cursor) => void;
-  onSourceChange: (s: string) => void;
+    source: string;
+    onCursorChange: (c: Cursor) => void;
+    onSourceChange: (s: string) => void;
 };
 
 function Editor(props: PropsWithChildren<OutputProps>) {
-  return (
-    <AceEditor
-      editorProps={{ $blockScrolling: true }}
-      fontSize={18}
-      width="100%"
-      height="82vh"
-      name="dada-editor"
-      onChange={(v) => props.onSourceChange(v)}
-      onCursorChange={(selection) => props.onCursorChange({ row: selection.cursor.row, column: selection.cursor.column })}
-      value={props.source}
-      theme="github"
-    />
-  );
+    const { colorMode } = useColorMode();
+    return (
+        <AceEditor
+            mode="rust"
+            editorProps={{ $blockScrolling: true }}
+            fontSize={18}
+            width="100%"
+            height="82vh"
+            name="dada-editor"
+            onChange={(v) => props.onSourceChange(v)}
+            onCursorChange={(selection) =>
+                props.onCursorChange({
+                    row: selection.cursor.row,
+                    column: selection.cursor.column,
+                })
+            }
+            value={props.source}
+            theme={colorMode === "dark" ? "twilight" : "github"}
+        />
+    );
 }
 
 export default Editor;
