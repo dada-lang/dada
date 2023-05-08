e0885d98d496966e60fcc37a4981bf2d522cecf4
Adam Andersson <adam.m.andersson@gmail.com>
2023-05-08 10:26:54 +0200

    start working on language server extension

---
+++ editors/code/README.md
@@ -0,0 +1,3 @@
+# Dada Language Server
+
+This vscode extension provides language support for the [Dada programming language](https://dada-lang.org/).
---
+++ editors/code/src/extension.ts
@@ -0,0 +1,41 @@
+import { ExtensionContext, WorkspaceConfiguration, workspace } from 'vscode';
+
+import {
+    Executable,
+    LanguageClient, LanguageClientOptions, ServerOptions,
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient | undefined = undefined;
+
+export function activate(_ctx: ExtensionContext) {
+    const config = workspace.getConfiguration("dadaLanguageServer");
+
+    const run: Executable = {
+        command: getExecutable(config),
+        args: ["ide"]
+    };
+    
+    const serverOptions: ServerOptions = {
+        run,
+        debug: run
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "dada" }],
+    };
+
+    client = new LanguageClient("dadaLanguageServer", "Dada Language Server", serverOptions, clientOptions);
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
+
+function getExecutable(config: WorkspaceConfiguration): string {
+    const explicitPath = process.env.__DADA_LSP_SERVER_DEBUG ?? config.get<string | null>("compiler.executablePath"); 
+    return explicitPath ?? "dada";
+}
