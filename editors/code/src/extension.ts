7be6e4e36b19bea50df40ffd18d40fa58b991f17
Adam Andersson <adam.m.andersson@gmail.com>
2023-05-12 11:16:16 +0200

    optional chaining

--- editors/code/src/extension.ts
+++ editors/code/src/extension.ts
@@ -37,10 +37,7 @@ export async function activate() {
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined;
-  }
-  return client.stop();
+  return client?.stop();
 }
 
 function getExecutable(config: WorkspaceConfiguration): string {
