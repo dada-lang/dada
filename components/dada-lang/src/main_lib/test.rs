3caf74fe257229026f190ccb4a7df4a78b3426c6
Niko Matsakis <niko@alum.mit.edu>
2024-08-30 06:08:16 -0400

    detect bogus arguments to test

--- components/dada-lang/src/main_lib/test.rs
+++ components/dada-lang/src/main_lib/test.rs
@@ -16,9 +16,9 @@ struct FailedTest {
 impl Main {
     pub(super) fn test(&mut self, options: &TestOptions) -> Fallible<()> {
         let tests = if options.inputs.is_empty() {
-            self.assemble_tests(&["."])
+            self.assemble_tests(&["."])?
         } else {
-            self.assemble_tests(&options.inputs)
+            self.assemble_tests(&options.inputs)?
         };
 
         eprintln!("Total tests: {}", tests.len());
@@ -41,23 +41,32 @@ impl Main {
         }
     }
 
-    fn assemble_tests(&self, inputs: &[impl AsRef<Path>]) -> Vec<PathBuf> {
-        inputs
-            .iter()
-            .flat_map(|input| {
-                let input: &Path = input.as_ref();
-                if input.extension().map(|e| e == "dada").unwrap_or(false) {
-                    vec![PathBuf::from(input)]
-                } else {
-                    WalkDir::new(input)
-                        .into_iter()
-                        .filter_map(|e| e.ok())
-                        .filter(|e| e.path().extension().map(|e| e == "dada").unwrap_or(false))
-                        .map(|e| e.into_path())
-                        .collect::<Vec<_>>()
+    fn assemble_tests(&self, inputs: &[impl AsRef<Path>]) -> Fallible<Vec<PathBuf>> {
+        let mut result = vec![];
+
+        for input in inputs {
+            let input: &Path = input.as_ref();
+
+            if !input.exists() {
+                bail!("test path '{}' does not exist", input.display());
+            } else if is_dada_file(input) {
+                result.push(input.to_path_buf());
+            } else if input.is_dir() {
+                for e in WalkDir::new(input) {
+                    let e = e?;
+                    if is_dada_file(e.path()) {
+                        result.push(e.into_path());
+                    }
                 }
-            })
-            .collect()
+            } else {
+                bail!(
+                    "input path '{}' is neither a .dada file nor a directory",
+                    input.display()
+                );
+            }
+        }
+
+        return Ok(result);
     }
 
     fn run_test(&self, input: &Path) -> Option<FailedTest> {
@@ -81,4 +90,8 @@ impl Main {
         }
         None
     }
+}
+
+fn is_dada_file(input: &Path) -> bool {
+    input.is_file() && input.extension().map(|e| e == "dada").unwrap_or(false)
 }
