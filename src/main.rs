a24ff44432a7875be308d796bf26baaf06a9590c
Niko Matsakis <niko@alum.mit.edu>
2024-10-06 07:30:26 -0400

    help diagnose stack overflows

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ use structopt::StructOpt;
 
 #[tokio::main]
 async fn main() -> Fallible<()> {
+    unsafe {
+        backtrace_on_stack_overflow::enable();
+    }
     dada_lang::Options::from_args().main()
 }
