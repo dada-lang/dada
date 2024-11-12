f19fd163c86db15c3ee8215e6ab4e303b0a99935
Niko Matsakis <niko@alum.mit.edu>
2024-11-12 10:52:21 +0000

    give up on wasm for now

--- components/vscode/src/extension.ts
+++ components/vscode/src/extension.ts
@@ -1,57 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
 import * as vscode from 'vscode';
-
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
-import { ExtensionContext, Uri, window, workspace, commands } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, RequestType } from 'vscode-languageclient/node';
-import { Wasm, ProcessOptions } from '@vscode/wasm-wasi';
-import { createStdioOptions, createUriConverters, startServer } from '@vscode/wasm-wasi-lsp';
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
-export async function activate(context: ExtensionContext) {
-	const wasm: Wasm = await Wasm.load();
+export function activate(context: vscode.ExtensionContext) {
+	// Path to the language server executable
+	const serverModule = context.asAbsolutePath("../../target/debug/dada-lsp-server");
 
-	const channel = window.createOutputChannel('LSP WASM Server');
-	const serverOptions: ServerOptions = async () => {
-		const options: ProcessOptions = {
-			stdio: createStdioOptions(),
-			mountPoints: [
-				{ kind: 'workspaceFolder' },
-			]
-		};
-		const filename = Uri.joinPath(context.extensionUri, '..', '..', 'target', 'wasm32-wasip1-threads', 'debug', 'dada-lsp-server.wasm');
-		const bits = await workspace.fs.readFile(filename);
-		const module = await WebAssembly.compile(bits);
-		const process = await wasm.createProcess('lsp-server', module, { initial: 160, maximum: 160, shared: true }, options);
-
-		const decoder = new TextDecoder('utf-8');
-		process.stderr!.onData((data) => {
-			channel.append(decoder.decode(data));
-		});
-
-		return startServer(process);
+	// Server options
+	const serverOptions: ServerOptions = {
+		run: { command: serverModule, transport: TransportKind.stdio },
+		debug: { command: serverModule, transport: TransportKind.stdio }
 	};
 
+	// Client options
 	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ language: 'plaintext' }],
-		outputChannel: channel,
-		uriConverters: createUriConverters(),
+		documentSelector: [{ scheme: 'file', language: 'dada' }],
+		synchronize: {
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/*.dada')
+		}
 	};
 
-	client = new LanguageClient('lspClient', 'LSP Client', serverOptions, clientOptions);
-	try {
-		await client.start();
-	} catch (error) {
-		client.error(`Start failed`, error, 'force');
-	}
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'dadaLanguageServer',
+		'Dada Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
 }
 
-export function deactivate() {
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
 	return client.stop();
 }
