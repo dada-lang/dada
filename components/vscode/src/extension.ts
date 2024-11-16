ec2adedfcaa5c4c94ed465a9ae3fc99de543a53e
Niko Matsakis <niko@alum.mit.edu>
2024-11-16 15:26:31 +0000

    integrate wasm experimentally

--- components/vscode/src/extension.ts
+++ components/vscode/src/extension.ts
@@ -1,42 +1,21 @@
-import * as path from 'path';
 import * as vscode from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+import * as wasmExtension from './wasm-extension';
+import * as nativeExtension from './native-extension';
 
-let client: LanguageClient;
+const USE_WASM = false;
 
 export function activate(context: vscode.ExtensionContext) {
-	// Path to the language server executable
-	const serverModule = context.asAbsolutePath("../../target/debug/dada-lsp-server");
-
-	// Server options
-	const serverOptions: ServerOptions = {
-		run: { command: serverModule, transport: TransportKind.stdio },
-		debug: { command: serverModule, transport: TransportKind.stdio }
-	};
-
-	// Client options
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'dada' }],
-		synchronize: {
-			fileEvents: vscode.workspace.createFileSystemWatcher('**/*.dada')
-		}
-	};
-
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'dadaLanguageServer',
-		'Dada Language Server',
-		serverOptions,
-		clientOptions
-	);
-
-	// Start the client. This will also launch the server
-	client.start();
+	if (USE_WASM) {
+		wasmExtension.activate(context);
+	} else {
+		nativeExtension.activate(context);
+	}
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
+	if (USE_WASM) {
+		return wasmExtension.deactivate();
+	} else {
+		return nativeExtension.deactivate();
 	}
-	return client.stop();
 }
